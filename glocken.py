73b45a58fea7042c0b61812cdc7b5847aada0e29
Kajotko <jotkarolina@gmail.com>
2016-12-13 14:26:16 +0000

    Update glocken.py

--- glocken.py
+++ glocken.py
@@ -4,6 +4,7 @@ import RPi.GPIO as GPIO
 # Library to slow or give a rest to the script
 import time
 
+# Assign pins for each musical note and the drum input
 E = 18  
 G = 23
 B = 17
@@ -15,13 +16,14 @@ GPIO.setup(E, GPIO.OUT)  # glockenspiel pins set as output
 GPIO.setup(G, GPIO.OUT)
 GPIO.setup(B, GPIO.OUT)
 GPIO.setup(D, GPIO.OUT)
-GPIO.setup(Drum, GPIO.IN,pull_up_down=GPIO.PUD_DOWN) #piezo pin set as input
+GPIO.setup(Drum, GPIO.IN,pull_up_down=GPIO.PUD_DOWN) #drum pin set as input
 
-GPIO.output(E, GPIO.LOW) #initial state of pins
+GPIO.output(E, GPIO.LOW) #initial state of pins to prevent solenoids form overheating
 GPIO.output(G, GPIO.LOW)
 GPIO.output(B, GPIO.LOW)
 GPIO.output(D, GPIO.LOW)
 
+#trancribe songs into a list of tuples which contain pitch and length of each note, then add them to playlist list
 playlist=[]
 loopsong_short=[(E,1),(G, 0.5), (G,0.5),(B,1),(G,1),(B,0.5),(D,0.5),(B, 0.5),(G,0.5),(B,2),(G,1),(B,0.5),(B,0.5),(D,1),(B,1),(D,0.5),(B,0.5),(G,0.5),(E,0.5),(G,2)]
 playlist.append(loopsong_short)
@@ -32,51 +34,53 @@ playlist.append(loopsong_long)
 mc_donald=[(G, 0.5),(G, 0.5),(G, 0.5),(E, 0.5),(B, 0.5),(B, 0.5),(G, 1),(D, 0.5),(D, 0.5),(B, 0.5),(B, 0.5),(G,2),(G, 0.5),(G, 0.5),(G, 0.5),(E, 0.5),(B, 0.5),(B, 0.5),(G,1),(D, 0.5),(D, 0.5),(B, 0.5),(B, 0.5),(G, 1.5),(E,0.5),(B, 0.5),(B, 0.5),(B, 0.5),(E, 0.5),(B, 0.5),(B, 0.5),(B, 0.5),(E, 0.5),(B, 0.5),(E,0.5),(B, 0.5),(E, 0.5),(B, 0.5),(E,0.5),(B, 0.5),(E, 0.5),(G, 0.5),(G,0.5),(G, 0.5),(E, 0.5),(B, 0.5),(B,0.5),(G, 1),(D,0.5),(D, 0.5),(B, 0.5),(B, 0.5),(G,2)]
 playlist.append(mc_donald)
 
+#Allow user to choose which song to play
 i=int(raw_input('Choose a song number:'))
 song=playlist[i-1]
+
+#Start a list to note timing of beats and define initial variables value
 beat_times=[]
-quiet=0
+quiet=0 
 note=0 #initial note, start playing from the beginning
 bar=0 #keeps track how many notes in bar have been played
-start_time = time.time()
+prev=0 #previous reading of the input pin, initially 0 before drum is hit
+start_time = time.time()#registers current time
+
+#Function to retrieve pitch and length of note from the list quickly
 def pitch(note):
     return song[note][0]
 def length(note):
     return song[note][1]
-
-prev=0
-
 
 try:
     while 1:
         # The input() function will return either a True or False
         # indicating whether the pin is HIGH or LOW.
 	drum=GPIO.input(Drum)
+	#Define conditions to interpret input as a hit
         if drum and prev==False:
 	    hit=True
 	    #print 'Hit'
         else:
 	    hit=False
 	    #print 'Nothing'
+	#overwrite previous reading with current reading to use in next loop iteration
 	prev=drum
-        if hit or bar!=int(bar):  #Piezo detects a hit or the next note has to come between hits
+        if hit or bar!=int(bar):  #Drum detects a hit or the next note has to come between hits
             if bar>=4: #reset bar if a full bar has been played
                 bar=0
-            if len(beat_times)>=2: #check if there have been at least 2 drum hits
-                halfbeat=(beat_times[-1]-beat_times[-2])/2 #calculate half the time between beats
-            else:
-                halfbeat=0.5
             if bar == int(bar): #check if drum was hit
                 start_time = time.time() #register time
                 beat_times.append(start_time)
-		if len(beat_times)<=4:
+		if len(beat_times)<=4: #do not play glockenspiel for the first 4 beats
 			time.sleep(0.1)
 			continue
-            else:
-                time.sleep(halfbeat) #if the glockenspiel wants to play note between drum beats have it wait
+            else: #if the drum was not hit but the glockenspiel wants to play note between drum beats
+		halfbeat=(beat_times[-1]-beat_times[-2])/2 #calculate half the time between beats
+                time.sleep(halfbeat) #have glockenspiel wait
             if note == len(song):  # when you've reached the last note reset
                 note = 0
-            if quiet: #do not play if the halfnote has just been played, advance song
+            if quiet: #do not play if a 2-beat note has just been played, advance song
                 quiet=0
                 note+=1
 		time.sleep(0.1)
@@ -87,14 +91,14 @@ try:
                 GPIO.output(pitch(note), GPIO.LOW)
                 bar += length(note)
             if length(note)>1: #check if a note longer than one has been played
-                quiet=1 #keep quiet if so...
+                quiet=1 #keep quiet for the next hit if so...
             else:
                 note+=1 #...advance song if not
             time.sleep(0.1)
-        else:  # Piezo sends no signal i.e no hit detected:
-            end_time = time.time()
-            elapsed_time = (end_time - start_time)  # * 1000000#measure time
-            if elapsed_time > 10:  # if the timer is over 10 s reset note
+        else:  #Drum pin signal Low
+            end_time = time.time() #note time now
+            elapsed_time = (end_time - start_time)  #measure time since last beat
+            if elapsed_time > 5:  # if drum has not been played in 5s reset song
                 note = 0
 
 
